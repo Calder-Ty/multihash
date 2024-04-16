e520f831050cf6fb371257b76176875603d4e0bf
Tyler Calder <calder-ty@protonmail.com>
2024-04-15 20:04:48 -0600

    feat: add Decoding

--- src/root.zig
+++ src/root.zig
@@ -28,13 +28,81 @@ const UnsignedVarInt = struct {
         // at a max 9 * 7 = 63 bits.
         return .{ ._inner = @truncate(result) };
     }
+
+    /// Give the minimal number of bytes need to represent
+    pub fn minimal_size(self: UnsignedVarInt) u8 {
+        inline for (1..9) |i| {
+            if (self._inner < (1 << 7 * i)) {
+                return i;
+            }
+        }
+        return 9;
+    }
+
+    fn decode(self: UnsignedVarInt) [9]?u8 {
+        var buffer = [_]?u8{null} ** 9;
+        const max_len = self.minimal_size();
+        for (0..9) |i| {
+            // SAFTEY: We can truncate here as `i` will never be greater than 9
+            const byte: u8 = @truncate(self._inner >> @as(u6, @truncate(i)) * 7);
+            if (i == max_len - 1) {
+                buffer[i] = byte;
+            } else {
+                buffer[i] = byte | 0b1000_0000;
+            }
+        }
+        return buffer;
+    }
 };
 
 test "encode" {
-    try std.testing.expectEqual(@as(u63, 1), (try UnsignedVarInt.encode(&[_]u8{0x1}))._inner);
-    try std.testing.expectEqual(@as(u63, 127), (try UnsignedVarInt.encode(&[_]u8{0x7f}))._inner);
-    try std.testing.expectEqual(@as(u63, 128), (try UnsignedVarInt.encode(&[_]u8{ 0x80, 0x01 }))._inner);
-    try std.testing.expectEqual(@as(u63, 255), (try UnsignedVarInt.encode(&[_]u8{ 0xff, 0x01 }))._inner);
-    try std.testing.expectEqual(@as(u63, 300), (try UnsignedVarInt.encode(&[_]u8{ 0xac, 0x02 }))._inner);
-    try std.testing.expectEqual(@as(u63, 16384), (try UnsignedVarInt.encode(&[_]u8{ 0x80, 0x80, 0x01 }))._inner);
+    try std.testing.expectEqual(
+        @as(u63, 1),
+        (try UnsignedVarInt.encode(&[_]u8{0x1}))._inner,
+    );
+    try std.testing.expectEqual(
+        @as(u63, 127),
+        (try UnsignedVarInt.encode(&[_]u8{0x7f}))._inner,
+    );
+    try std.testing.expectEqual(
+        @as(u63, 128),
+        (try UnsignedVarInt.encode(&[_]u8{ 0x80, 0x01 }))._inner,
+    );
+    try std.testing.expectEqual(
+        @as(u63, 255),
+        (try UnsignedVarInt.encode(&[_]u8{ 0xff, 0x01 }))._inner,
+    );
+    try std.testing.expectEqual(
+        @as(u63, 300),
+        (try UnsignedVarInt.encode(&[_]u8{ 0xac, 0x02 }))._inner,
+    );
+    try std.testing.expectEqual(
+        @as(u63, 16384),
+        (try UnsignedVarInt.encode(&[_]u8{ 0x80, 0x80, 0x01 }))._inner,
+    );
+}
+
+test "mimimal bytes" {
+    try std.testing.expectEqual(1, (UnsignedVarInt{ ._inner = 1 }).minimal_size());
+    try std.testing.expectEqual(3, (UnsignedVarInt{ ._inner = 16384 }).minimal_size());
+    try std.testing.expectEqual(9, (UnsignedVarInt{ ._inner = 1 << 62 }).minimal_size());
+}
+
+test "decode" {
+    const int = UnsignedVarInt{ ._inner = 0x1 };
+    try std.testing.expectEqualSlices(
+        ?u8,
+        &[_]?u8{0x1},
+        int.decode()[0..int.minimal_size()],
+    );
+    const int2 = UnsignedVarInt{ ._inner = 16384 };
+    try std.testing.expectEqualSlices(
+        ?u8,
+        &[_]?u8{
+            0x80,
+            0x80,
+            0x01,
+        },
+        int2.decode()[0..int2.minimal_size()],
+    );
 }
